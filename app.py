cc89f33d29f2ee1110ece94855115fa41902c80b
sharexy23 <omotietie@yahoo.com>
2021-01-21 13:16:46 +0100

    hsgscg

--- app.py
+++ app.py
@@ -13,7 +13,8 @@ app.secret_key = '!@#$%^&*()_+=-0987654321'
 app.config['MAIL_SERVER']='smtp.gmail.com'
 app.config['MAIL_PORT']= 587
 app.config['MAIL_USE_TLS']= True
-app.config['MAIL_USE_SSL']= True
+app.config['MAIL_USE_SSL']= False
+app.config['MAIL_DEBUG']= True
 app.config['MAIL_USERNAME']='iyowolabi@gmail.com'
 app.config['MAIL_PASSWORD']='temi1967'
 
