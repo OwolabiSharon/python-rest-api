f3ccd341bb8d9e79cc2784bd1a850fa8273bc676
sharexy23 <omotietie@yahoo.com>
2021-01-30 00:42:44 +0100

    hopefully final commit

--- app.py
+++ app.py
@@ -95,14 +95,12 @@ class Email(Resource):
         global users
         data = Email.parser.parse_args()
 
-        #user = next(filter(lambda x: x['email'] == data['email'] , users))
+        user = next(filter(lambda x: x.email == data['email'] , users))
         if User.find_by_email(data['email']):
             return {"message": "A user with this email as already been verified and is saved in our database"}, 400
 
-        elif data['verification_code'] in numbers:
-            for user in users:
-                if user.email == data['email']:
-                    User.save_to_db(user)
+        elif data['verification_code'] in numbers and user.email == data['email']:
+            User.save_to_db(user)
             return {'message':'now you are verified and saved to our database'}
 
 
